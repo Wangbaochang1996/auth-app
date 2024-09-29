da781547381c95a64327746b79ad294d788636de
Wangbaochang1996 <18338190901@163.com>
2024-09-29 13:50:36 +0800

    forgot password

--- lib/forgot_password.dart
+++ lib/forgot_password.dart
@@ -17,14 +17,14 @@ class _ForgotPasswordState extends State<ForgotPassword> {
   resetPassword() async {
     try {
       await FirebaseAuth.instance.sendPasswordResetEmail(email: email);
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
           content: Text(
         "Password Reset Email has been sent!",
         style: TextStyle(fontSize: 20.0),
       )));
     } on FirebaseAuthException catch (e) {
       if (e.code == "user-not-found") {
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
             content: Text(
           "No user found for that email",
           style: TextStyle(fontSize: 20),
@@ -40,12 +40,12 @@ class _ForgotPasswordState extends State<ForgotPassword> {
       body: Container(
         child: Column(
           children: [
-            SizedBox(
+            const SizedBox(
               height: 70,
             ),
             Container(
               alignment: Alignment.topCenter,
-              child: Text(
+              child: const Text(
                 "Password Recovery",
                 style: TextStyle(
                     color: Colors.white,
@@ -53,8 +53,8 @@ class _ForgotPasswordState extends State<ForgotPassword> {
                     fontWeight: FontWeight.bold),
               ),
             ),
-            SizedBox(height: 10),
-            Text(
+            const SizedBox(height: 10),
+            const Text(
               "Enter your mail",
               style: TextStyle(
                   color: Colors.white,
@@ -65,11 +65,11 @@ class _ForgotPasswordState extends State<ForgotPassword> {
                 child: Form(
               key: _formkey,
               child: Padding(
-                padding: EdgeInsets.only(left: 10),
+                padding: const EdgeInsets.only(left: 10),
                 child: ListView(
                   children: [
                     Container(
-                      padding: EdgeInsets.only(left: 10),
+                      padding: const EdgeInsets.only(left: 10),
                       decoration: BoxDecoration(
                         border: Border.all(color: Colors.white, width: 2.0),
                         borderRadius: BorderRadius.circular(30),
@@ -82,8 +82,8 @@ class _ForgotPasswordState extends State<ForgotPassword> {
                           return null;
                         },
                         controller: mailcontroller,
-                        style: TextStyle(color: Colors.white),
-                        decoration: InputDecoration(
+                        style: const TextStyle(color: Colors.white),
+                        decoration: const InputDecoration(
                           border: InputBorder.none,
                           hintText: 'Email',
                           hintStyle: TextStyle(
@@ -98,7 +98,7 @@ class _ForgotPasswordState extends State<ForgotPassword> {
                         ),
                       ),
                     ),
-                    SizedBox(
+                    const SizedBox(
                       height: 40.0,
                     ),
                     GestureDetector(
@@ -112,35 +112,37 @@ class _ForgotPasswordState extends State<ForgotPassword> {
                       },
                       child: Container(
                         width: MediaQuery.of(context).size.width,
-                        padding: EdgeInsets.all(10),
+                        padding: const EdgeInsets.all(10),
                         decoration: BoxDecoration(
                           color: Colors.white,
                           borderRadius: BorderRadius.circular(30),
                         ),
-                        child: Text(
-                          "Send Email",
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 18.0,
-                            fontWeight: FontWeight.w500,
+                        child: Center(
+                          child: Text(
+                            "Send Email",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.w500,
+                            ),
                           ),
                         ),
                       ),
                     ),
-                    SizedBox(
+                    const SizedBox(
                       height: 50,
                     ),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Text(
+                        const Text(
                           "Don't have a account?",
                           style: TextStyle(
                             fontSize: 18,
                             color: Colors.white,
                           ),
                         ),
-                        SizedBox(
+                        const SizedBox(
                           width: 5,
                         ),
                         GestureDetector(
@@ -148,9 +150,9 @@ class _ForgotPasswordState extends State<ForgotPassword> {
                             Navigator.push(
                                 context,
                                 MaterialPageRoute(
-                                    builder: (context) => SignUp()));
+                                    builder: (context) => const SignUp()));
                           },
-                          child: Text(
+                          child: const Text(
                             "Create",
                             style: TextStyle(
                               color: Colors.green,
